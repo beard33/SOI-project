8261cfb0025921bdf25f019f8e6cd3afcb568c62
AlbertoCippelletti <acippelletti@gmail.com>
2021-02-16 22:02:01 +0000

    Add files via upload

--- front-end/login/login.js
+++ front-end/login/login.js
@@ -13,3 +13,82 @@ function login(){
     reg.style.left = "450px";
     btn.style.left = "0px";
 }
+
+/* controllo inserimento password */
+var myInput = document.getElementById("password");
+var letter = document.getElementById("letter");
+var capital = document.getElementById("capital");
+var number = document.getElementById("number");
+var length = document.getElementById("length");
+var check_passw_lowecase = false; 
+var check_passw_lowecase = false;
+var check_passw_uppercase = false;
+var check_passw_number = false;
+var check_passw_length = false;
+var check_password = false;
+
+// When the user clicks on the password field, show the message box
+myInput.onfocus = function() {
+  document.getElementById("message").style.display = "block";
+}
+
+// When the user clicks outside of the password field, hide the message box
+myInput.onblur = function() {
+  document.getElementById("message").style.display = "none";
+}
+
+// When the user starts to type something inside the password field
+myInput.onkeyup = function() {
+  // Validate lowercase letters
+  var lowerCaseLetters = /[a-z]/g;
+  if(myInput.value.match(lowerCaseLetters)) {
+    check_passw_lowecase = true;
+    } else {
+        check_passw_lowecase = false;
+    }
+
+  // Validate capital letters
+  var upperCaseLetters = /[A-Z]/g;
+  if(myInput.value.match(upperCaseLetters)) {
+    check_passw_uppercase = true;
+    } else {
+        check_passw_uppercase = false;
+    }
+
+  // Validate numbers
+  var numbers = /[0-9]/g;
+  if(myInput.value.match(numbers)) {
+    check_passw_number = true;
+    } else {
+        check_passw_number = false;
+    }
+
+  // Validate length
+  if(myInput.value.length >= 8) {
+    check_passw_length = true;
+    } else {
+        check_passw_length = false;
+    }
+
+    if(check_passw_length == true && check_passw_lowecase == true && check_passw_number == true && check_passw_uppercase == true){
+        document.getElementById('control_message').style.color = 'green';
+        document.getElementById('control_message').innerHTML = 'password corretta'; 
+        check_password = true;
+    } else{
+        document.getElementById('control_message').style.color = 'red';
+        document.getElementById('control_message').innerHTML = 'password errata';
+        check_password = false;
+    }
+}
+
+/* funzione per il controllo sul reinserimento della password */
+var check = function() {
+    if (document.getElementById('password').value ==
+      document.getElementById('confirm_password').value && check_password == true) {
+      document.getElementById('control_message').style.color = 'green';
+      document.getElementById('control_message').innerHTML = 'password corretta';
+    } else {
+      document.getElementById('control_message').style.color = 'red';
+      document.getElementById('control_message').innerHTML = 'password errata';
+    }
+  }

1cf0a56b1c5ca2eaaa33d551a7f0299c1e9dd390
Beard33 <andreaguzzon26@gmail.com>
2021-03-03 12:23:46 +0100

    Add home/new-project navigability
    The 'page'<div> now updates accordingly to hashchange
    Navbar css also adapt to context

--- front-end/DASHBOARD/home.js
+++ front-end/DASHBOARD/home.js
@@ -1,31 +1,50 @@
 $(document).ready(function() {
+    renderPage();
+    $(window).on('hashchange', renderPage);
+})
 
-    $('#new-project').click(function(event) {
-        event.preventDefault();
+function renderPage() {
+    const pageName = (window.location.hash);
+    switch (pageName) {
+        case '#new-project':
+            renderNewProject();
+            break;
+        case '#home':
+            renderHome();
+            break;
+    }
+}
 
-        $('#title').html("Create new project");
+function renderNewProject(){
+    $('#title').html("Create new project");
+    $('#new-project-btn').css('text-decoration', 'underline').css('color', '#e7e7c8');;
+    $('#home-btn').css('text-decoration', 'none').css('color', '#ecf0f1');
 
-        $('.template').empty().append(
-            `<div id="project-title">
-                <p>Project name:</p>
-                <input name="project-name" type="text" size="40" placeholder="Project name" required>
-            </div>
-            <div id="project-type">
-                <p>Project type:</p>
-                <input name="project-type" id="photo-type" type="radio" value="Photo"> 
-                <label for="photo-type">Photo</label>
-                <input name="project-type" id="text-type" type="radio" value="Images">
-                <label for="text-type">Text</label>
-            </div>
-            <div id="project-files">
-                <p>Add files:</p>
-                <input name="project-files" type="file" multiple>
-            </div>
-            <div id="confirm-project">
-                <button type="button">Create project</button>
-            </div>`
-        );
-    })
+    $('.page').empty().append(
+        `<div id="project-title">
+            <p>Project name:</p>
+            <input name="project-name" type="text" size="40" placeholder="Project name" required>
+        </div>
+        <div id="project-type">
+            <p>Project type:</p>
+            <input name="project-type" id="photo-type" type="radio" value="Photo"> 
+            <label for="photo-type">Photo</label>
+            <input name="project-type" id="text-type" type="radio" value="Images">
+            <label for="text-type">Text</label>
+        </div>
+        <div id="project-files">
+            <p>Add files:</p>
+            <input name="project-files" type="file" multiple>
+        </div>
+        <div id="confirm-project">
+            <button type="button">Create project</button>
+        </div>`
+    );
+}
 
-});
-
+function renderHome(){
+    $('#title').html("Project list");
+    $('#new-project-btn').css('text-decoration', 'none').css('color', '#ecf0f1');
+    $('#home-btn').css('text-decoration', 'underline').css('color', '#e7e7c8');
+    $('.page').empty();
+}
